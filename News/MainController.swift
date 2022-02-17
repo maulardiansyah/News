28c7027c1b988c8bbe281a3aeec617b60e68f398
Maulana Ardiansyah <maul.ardiansyahh@gmail.com>
2022-02-17 21:36:16 +0700

    Initial Commit

---
+++ News/MainController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Maul on 17/02/22.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
